de24af5545da061d5a801e28a14dfd0377cdd4d0
Alexander Burias <alexander.burias@gmail.com>
2015-10-22 20:06:26 +0800

    Group retreival for a specific user.

--- BH.Tests/UserGroupTests.cs
+++ BH.Tests/UserGroupTests.cs
@@ -61,5 +61,58 @@ namespace BH.Tests
             userManager.Delete(newUser);
             groupManager.Delete(group.Id);
         }
+
+        [TestMethod]
+        public void Can_Get_My_Child_Groups()
+        {
+            var existingUser = userManager.FindByName("testuser");
+            if (existingUser != null)
+                userManager.Delete(existingUser);
+
+            var existingGroup = groupManager.GetByName("Group A");
+            if (existingGroup != null)
+                groupManager.Delete(existingGroup.Id);
+
+            var existingGroup1 = groupManager.GetByName("Group B");
+            if (existingGroup1 != null)
+                groupManager.Delete(existingGroup1.Id);
+
+            var newUser = new User()
+            {
+                FirstName = "Test",
+                LastName = "User",
+                EmailConfirmed = true,
+                Email = "test@user.com",
+                UserName = "testuser"
+            };
+            userManager.Create(newUser, "password");
+
+            var group = new Group() { Name = "Group A" };
+            groupManager.Create(group);
+
+            var group1 = new Group() {Name = "Group B"};
+            groupManager.Create(group1);
+
+            groupManager.AssignParent(group1, group);
+
+            Assert.IsTrue(groupManager.AddUser(group.Id, newUser.Id));
+
+            var userGroups = userManager.GetById(newUser.Id, new List<string>() {"Groups", "Groups.ChildGroups"});
+            var allMyGroups = new List<Group>();
+            foreach (var myGroup in userGroups.Groups)
+            {
+                allMyGroups.Add(myGroup);
+                allMyGroups.AddRange(myGroup.ChildGroups);
+            }
+
+            Console.WriteLine("All My Groups I Can View Below!");
+            foreach (var allMyGroup in allMyGroups)
+                Console.WriteLine("Group Name: " + allMyGroup.Name);
+
+
+            userManager.Delete(newUser);
+            groupManager.Delete(group.Id);
+            groupManager.Delete(group1.Id);
+        }
     }
 }
